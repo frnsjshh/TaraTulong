e02c9c232fc430f820c3642213c3e9756f763344
Francis <144906587+frnsjshh@users.noreply.github.com>
2026-09-03 15:05:38 +0800

    Remove unnecessary Maven build step from CI workflow and clean up test class formatting

--- src/test/java/com/francis/taratulong/TaraTulongApplicationTests.java
+++ src/test/java/com/francis/taratulong/TaraTulongApplicationTests.java
@@ -1,13 +1,9 @@
 package com.francis.taratulong;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 @SpringBootTest
 class TaraTulongApplicationTests {
-
     @Test
     void contextLoads() {
     }
-
 }
