74bb90bce9398c1cb93461e9990527c87c63ab18
Francis <144906587+frnsjshh@users.noreply.github.com>
2026-09-24 16:14:17 +0800

    Add `provinceId` field to `Location` entity and update schema with migration

--- src/main/resources/db/migration/V4__add_regionId_municipalityId_provinceId.sql
+++ src/main/resources/db/migration/V4__add_regionId_municipalityId_provinceId.sql
@@ -2,10 +2,16 @@ ALTER TABLE location
     ADD municipality_id INTEGER;
 
 ALTER TABLE location
+    ADD province_id INTEGER;
+
+ALTER TABLE location
     ADD region_id INTEGER;
 
 ALTER TABLE location
     ALTER COLUMN municipality_id SET NOT NULL;
+
+ALTER TABLE location
+    ALTER COLUMN province_id SET NOT NULL;
 
 ALTER TABLE location
     ALTER COLUMN region_id SET NOT NULL;
