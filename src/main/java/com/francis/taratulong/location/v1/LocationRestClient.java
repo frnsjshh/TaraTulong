62a237754116e099762869d6150926167c34e7b3
Francis <144906587+frnsjshh@users.noreply.github.com>
2026-09-24 15:48:36 +0800

    Stable RestClient

--- src/main/java/com/francis/taratulong/location/v1/LocationRestClient.java
+++ src/main/java/com/francis/taratulong/location/v1/LocationRestClient.java
@@ -2,7 +2,7 @@ package com.francis.taratulong.location.v1;
 
 
 import com.francis.taratulong.location.v1.dto.PsgcApiResponse;
-import com.francis.taratulong.location.v1.dto.PsgcRegionResponseDTO;
+import com.francis.taratulong.location.v1.dto.PsgcResponseDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
@@ -26,18 +26,18 @@ public class LocationRestClient {
         this.apiKey = apiKey;
     }
 
-    public List<PsgcRegionResponseDTO> fetchRegion() {
-        log.info("Fetching region from PSGC API");
+    public List<PsgcResponseDTO> fetchRegions() {
+        log.info("Fetching regions from PSGC API");
 
         try {
-            PsgcApiResponse<PsgcRegionResponseDTO> response = restClient.get()
+            PsgcApiResponse<PsgcResponseDTO> response = restClient.get()
                     .uri(uriBuilder -> uriBuilder
                             .path("/regions")
                             .queryParam("token", apiKey)
                             .build())
                     .retrieve()
-                    .body(new ParameterizedTypeReference<PsgcApiResponse<PsgcRegionResponseDTO>>() {});
-            List<PsgcRegionResponseDTO> regions = (response !=null && response.results() != null)
+                    .body(new ParameterizedTypeReference<PsgcApiResponse<PsgcResponseDTO>>() {});
+            List<PsgcResponseDTO> regions = (response !=null && response.results() != null)
                     ? response.results()
                     : List.of();
             log.info("Successfully fetched {} regions from PSGC API", regions.size());
@@ -47,5 +47,46 @@ public class LocationRestClient {
             throw e;
         }
     }
+
+    public List<PsgcResponseDTO> fetchProvinces(int regionId) {
+        log.info("Fetching provinces from PSGC API");
+        try {
+            PsgcApiResponse<PsgcResponseDTO> response = restClient.get()
+                    .uri(uriBuilder -> uriBuilder
+                            .path("/provinces")
+                            .queryParam("token", apiKey)
+                            .queryParam("region", regionId)
+                            .build())
+                    .retrieve()
+                    .body(new ParameterizedTypeReference<PsgcApiResponse<PsgcResponseDTO>>() {});
+            List<PsgcResponseDTO> provinces = (response != null && response.results() != null)
+                    ? response.results()
+                    : List.of();
+            log.info("Successfully fetched {} provinces from PSGC API", provinces.size());
+            return provinces;
+        } catch (Exception e) {
+            log.error("Error fetching province from PSGC API: {}", e.getMessage(), e);
+            throw e;
+        }
+    }
+
+    public List<PsgcResponseDTO> restRequest(String path, String queryParam, String arg) {
+        PsgcApiResponse<PsgcResponseDTO> apiResponse = restClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path(path)
+                        .queryParam("token", apiKey)
+                        .queryParam(queryParam, arg)
+                        .build())
+                .retrieve()
+                .body(new ParameterizedTypeReference<PsgcApiResponse<PsgcResponseDTO>>() {});
+        return (apiResponse != null && apiResponse.results() != null)
+                ? apiResponse.results()
+                : List.of();
+    }
+
+
+
+
+
 
 }
